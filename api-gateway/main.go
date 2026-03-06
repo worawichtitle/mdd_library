603fe820c87f76232541c06e59ec65abdc42cb79
PeerachM <66070301@kmitl.ac.th>
2026-03-06 23:20:45 +0700

    feat: add API gateway

---
+++ api-gateway/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	BorrowServiceURL  = "http://borrow-return:8081"
+	CatalogServiceURL = "http://book-catalog:8082"
+	UserServiceURL    = "http://user-management:8083"
+)
+
+// Reverse Proxy
+func reverseProxy(target string) gin.HandlerFunc {
+	targetURL, _ := url.Parse(target)
+	proxy := httputil.NewSingleHostReverseProxy(targetURL)
+
+	return func(c *gin.Context) {
+		proxy.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+func main() {
+	r := gin.Default()
+
+
+	r.Any("/users", reverseProxy(UserServiceURL))
+	r.Any("/users/*filepath", reverseProxy(UserServiceURL))
+
+
+	r.Any("/books", reverseProxy(CatalogServiceURL))
+	r.Any("/books/*filepath", reverseProxy(CatalogServiceURL))
+
+	r.Any("/borrows", reverseProxy(BorrowServiceURL))
+	r.Any("/borrows/*filepath", reverseProxy(BorrowServiceURL))
+
+	log.Println("API Gateway is running on port 8000...")
+	r.Run(":8000")
+}
